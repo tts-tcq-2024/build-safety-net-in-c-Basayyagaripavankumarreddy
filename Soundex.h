5ed0ba8c9aefe44f7c06f62901cc0145a02487f4
Basayyagaripavankumarreddy <Basayyagaripavankumarreddy@gmail.com>
2024-09-02 16:04:35 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,64 +1,47 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-
-void InitializeTheSoundex(const char* name, char* soundex);
-void AppendingSoundexCharacters(const char* name, char* soundex, char* prevCode);
-void Characters(char character, char* soundex, char* prevCode);
-int AppendCode(char code, char prevCode);
-void SoundexCode(char* soundex);
+ 
+void InitializeTheSoundex(char name[], char soundex[]);
+void AppendingSoundexCharacters(char name[], char soundex[], char *prevCode);
+void Characters(char character, char soundex[], char *prevCode);
 char GetSoundexCode(char character);
-void GenerateSoundex(const char* name, char* soundex);
-
-void GenerateSoundex(const char* name, char* soundex) {
+int AppendCode(char code, char prevCode);
+void SoundexCode(char soundex[]);
+ 
+void GenerateSoundex(char name[], char soundex[]) {
     if (name == NULL || strlen(name) == 0) {
-        strcpy(soundex, "");
+        soundex[0] = '\0';
         return;
     }
-    
+ 
     InitializeTheSoundex(name, soundex);
     char prevCode = GetSoundexCode(name[0]);
-    
     AppendingSoundexCharacters(name, soundex, &prevCode);
     SoundexCode(soundex);
 }
-
-void InitializeTheSoundex(const char* name, char* soundex) {
+ 
+void InitializeTheSoundex(char name[], char soundex[]) {
     soundex[0] = toupper(name[0]);
     soundex[1] = '\0';
 }
-
-void AppendingSoundexCharacters(const char* name, char* soundex, char* prevCode) {
-    int length = strlen(name);
-    for (int i = 1; i < length && strlen(soundex) < 4; i++) {
+ 
+void AppendingSoundexCharacters(char name[], char soundex[], char *prevCode) {
+    for (int i = 1; i < strlen(name) && strlen(soundex) < 4; i++) {
         Characters(name[i], soundex, prevCode);
     }
 }
-
-void Characters(char character, char* soundex, char* prevCode) {
+ 
+void Characters(char character, char soundex[], char *prevCode) {
     if (isalpha(character)) {
         char code = GetSoundexCode(character);
         if (AppendCode(code, *prevCode)) {
-            int len = strlen(soundex);
-            soundex[len] = code;
-            soundex[len + 1] = '\0';
+            strncat(soundex, &code, 1);
             *prevCode = code;
         }
     }
 }
-
-int AppendCode(char code, char prevCode) {
-    return code != '0' && code != prevCode;
-}
-
-void SoundexCode(char* soundex) {
-    while (strlen(soundex) < 4) {
-        int len = strlen(soundex);
-        soundex[len] = '0';
-        soundex[len + 1] = '\0';
-    }
-}
-
+ 
 char GetSoundexCode(char character) {
     character = toupper(character);
     switch (character) {
@@ -78,17 +61,13 @@ char GetSoundexCode(char character) {
             return '0';
     }
 }
-
-int main() {
-    char name[100];
-    char soundex[5];
-
-    printf("Enter a name: ");
-    scanf("%s", name);
-
-    GenerateSoundex(name, soundex);
-
-    printf("Soundex code: %s\n", soundex);
-
-    return 0;
+ 
+int AppendCode(char code, char prevCode) {
+    return code != '0' && code != prevCode;
+}
+ 
+void SoundexCode(char soundex[]) {
+    while (strlen(soundex) < 4) {
+        strncat(soundex, "0", 1);
+    }
 }
